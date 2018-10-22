e603927dac4d61a0122de7b4688582036b1556a6
Larry Glebov <larry.glebov@gmail.com>
2018-10-22 12:14:40 +0200

    added new site

---
+++ precache-manifest.30777a2ce080ee3be2a7c8965b4fc40e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f1fc6757383e3c9e0957",
+    "url": "/static/js/main.f1fc6757.chunk.js"
+  },
+  {
+    "revision": "160af9e4660554e6e041",
+    "url": "/static/js/1.160af9e4.chunk.js"
+  },
+  {
+    "revision": "f1fc6757383e3c9e0957",
+    "url": "/static/css/main.f02cf2d3.chunk.css"
+  },
+  {
+    "revision": "0e0e6d2e32b2c7d420121f713e8faf0a",
+    "url": "/index.html"
+  }
+];
